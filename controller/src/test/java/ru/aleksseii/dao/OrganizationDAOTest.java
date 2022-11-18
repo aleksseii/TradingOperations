8bf3f8382b443d6c37a1610e73ea079f0795f52c
aleksseii <alesha.shkel@mail.ru>
2022-11-18 03:05:56 +0300

    rewritten OrganizationDAO with jOOQ and ActiveRecords, also refactored its test

--- controller/src/test/java/ru/aleksseii/dao/OrganizationDAOTest.java
+++ controller/src/test/java/ru/aleksseii/dao/OrganizationDAOTest.java
@@ -1,5 +1,6 @@
 package ru.aleksseii.dao;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.jetbrains.annotations.NotNull;
@@ -7,12 +8,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.aleksseii.database.ConnectionManager;
+import ru.aleksseii.database.DataSourceManager;
 import ru.aleksseii.database.FlywayInitializer;
 import ru.aleksseii.model.Organization;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,22 +39,16 @@ public final class OrganizationDAOTest {
             EXISTING_ORG2,
             new Organization(3, 333L, "org_3", "acc_3")
     );
+    private static final @NotNull HikariDataSource DATA_SOURCE = DataSourceManager.getHikariDataSource();
 
-
-    private static final @NotNull Connection CONNECTION = ConnectionManager.getConnectionOrThrow();
-
-    private final @NotNull OrganizationDAO organizationDAO = new OrganizationDAO(CONNECTION);
+    private final @NotNull OrganizationDAO organizationDAO = new OrganizationDAO(DATA_SOURCE);
 
 
     @AfterAll
     static void closeConnection() {
 
         FlywayInitializer.initDB();
-        try {
-            CONNECTION.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        DATA_SOURCE.close();
     }
 
     @BeforeEach
